76dcab0c8303aff4cd0749d91b1ff5a011ffb369
Shreyas Wani <78205246+shreyaswani@users.noreply.github.com>
2024-12-31 11:40:26 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const http = require("http");
 const express = require("express");
 const socketio = require("socket.io");
 const formatMessage = require("./utils/messages");
-const createAdapter = require("@socket.io/redis-adapter").createAdapter;
-const redis = require("redis");
+// const createAdapter = require("@socket.io/redis-adapter").createAdapter;
+// const redis = require("redis");
 require("dotenv").config();
-const { createClient } = redis;
+// const { createClient } = redis;
 const {
   userJoin,
   getCurrentUser,
@@ -29,12 +29,12 @@ app.use(express.static(path.join(__dirname, "public")));
 
 const botName = "Admin";
 
-(async () => {
-  pubClient = createClient({ url: "redis://127.0.0.1:6379" });
-  await pubClient.connect();
-  subClient = pubClient.duplicate();
-  io.adapter(createAdapter(pubClient, subClient));
-})();
+// (async () => {
+//   pubClient = createClient({ url: "redis://127.0.0.1:6379" });
+//   await pubClient.connect();
+//   subClient = pubClient.duplicate();
+//   io.adapter(createAdapter(pubClient, subClient));
+// })();
 
 // Run when client connects
 io.on("connection", (socket) => {
